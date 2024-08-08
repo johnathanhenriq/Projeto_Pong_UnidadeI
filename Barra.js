58b50e7b58a8ad766369972ba988dd4a0cdf62fc
miqueias <miqueias.nilton12@hotmail.com>
2024-08-08 16:40:43 -0300

    Alteração README

---
+++ Barra.js
@@ -0,0 +1,11 @@
+class Barra {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = 15;
+    }
+
+
+}
