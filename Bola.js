b3bf6c36c2a9817d571fcc29c2aaf7f1c0772151
WilianeLima <142109280+WilianeLima@users.noreply.github.com>
2024-08-08 00:53:48 -0300

    Bola.js
    Mudança de cor e velocidade da bola.

--- Bola.js
+++ Bola.js
@@ -8,7 +8,7 @@ class Bola {
     }
 
     draw(context) {
-        context.fillStyle = 'white';
+        context.fillStyle = 'black';
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         context.closePath();
