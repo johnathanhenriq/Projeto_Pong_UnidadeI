3856500f0e4fbef6792d70f30771846eb8b20c65
miqueias <miqueias.nilton12@hotmail.com>
2024-09-25 14:08:19 -0300

    Barra

--- Barra.js
+++ Barra.js
@@ -28,6 +28,19 @@ class Barra {
 
     diminuirAltura(fator) {
         this.height *= fator;
-        this.height = Math.max(this.height, 10); 
+        this.height = Math.max(this.height, 10); // altura mínima 
+    }
+
+    aumentarAltura(fator) {
+        this.height *= fator;
+        this.height = Math.min(this.height, this.canvas.height - 20); // altura máxima
+    }
+
+    aplicarRecompensa(recompensa) {
+        if (recompensa.tipo === 'amarelo') {
+            this.aumentarAltura(1.2); // Aumenta a altura em 20%
+        } else if (recompensa.tipo === 'vermelho') {
+            this.diminuirAltura(0.8); // Diminui a altura em 20%
+        }
     }
 }
